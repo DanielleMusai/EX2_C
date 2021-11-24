203329c8801a7aee86d5509a6eb1afb3e825d597
DanielleMusai <92378800+DanielleMusai@users.noreply.github.com>
2021-11-24 09:54:45 +0200

    Update my_mat.h

--- my_mat.h
+++ my_mat.h
@@ -1,8 +1,8 @@
 #define my_mat
 #include <stdio.h>
 #define n 10
-void Matrix(int matrix[n][n]);
-int pathOrnot(int mat[n][n],int i, int j);
-void valueMatrix(int mat[n][n],int i, int j);
-int min(int a, int b);
+void A(int matrix[n][n]);
+int B(int mat[n][n],int i, int j);
+void C(int mat[n][n],int i, int j);
+int minFunc(int a, int b);
 void theShortestpath(int mat[n][n]);

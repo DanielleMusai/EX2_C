0c8d46460bcce4d6b2144ace9d1060e7e30092d4
DanielleMusai <92378800+DanielleMusai@users.noreply.github.com>
2021-11-24 09:31:06 +0200

    Create my_mat.h

---
+++ my_mat.h
@@ -0,0 +1,8 @@
+#define my_mat
+#include <stdio.h>
+#define n 10
+void Matrix(int matrix[n][n]);
+int pathOrnot(int mat[n][n],int i, int j);
+void valueMatrix(int mat[n][n],int i, int j);
+int min(int a, int b);
+void theShortestpath(int mat[n][n]);
